4e52f31c0cb28f824ad220a16fee705bfa95326e
alexayuxui <113915946+alexayuxui@users.noreply.github.com>
2022-12-09 04:05:17 +0800

    add read more read less

--- js/scripts.js
+++ js/scripts.js
@@ -8,3 +8,21 @@ function menuToggle() {
       x.className = 'navtoggle';
     }
   }
+
+
+const readMoreBtn = document.querySelector(".read-more-btn");
+const text = document.querySelector(".text");
+
+readMoreBtn.addEventListener("click",(e)=>{
+  text.classList.toggle("show-more");
+  if(readMoreBtn.innerText === "Read More"){
+    readMoreBtn.innerText = "Read Less";
+  }else{
+    readMoreBtn.innerText = "Read More";
+  }
+
+
+
+
+
+})
