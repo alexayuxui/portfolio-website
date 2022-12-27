1acf71ae9dd6a5066eb1dc0ebcc4ece384e91f1e
alexayuxui <113915946+alexayuxui@users.noreply.github.com>
2022-12-27 11:42:31 +0800

    Layout update

--- js/scripts.js
+++ js/scripts.js
@@ -10,25 +10,40 @@ function menuToggle() {
 }
 
 
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+}
+
 
 
   
-const readMoreBtn = document.querySelectorAll(".read-more-btn");
-const text = document.querySelectorAll(".text");
+// const readMoreBtn = document.querySelectorAll(".read-more-btn");
+// const text = document.querySelectorAll(".text");
 
-for (let i = 0; i < readMoreBtn.length; i++) {
-  readMoreBtn[i].addEventListener("click", (e)=>{
-    text[i].classList.toggle("show-more");
-    if(readMoreBtn[i].innerText != "View Less"){
-      readMoreBtn[i].innerText = "View Less";
-    }else{
-      readMoreBtn[i].innerText = "View Again";
-    }
-
-  });
-
-
-}
+// for (let i = 0; i < readMoreBtn.length; i++) {
+//   readMoreBtn[i].addEventListener("click", (e)=>{
+//     text[i].classList.toggle("show-more");
+//     if(readMoreBtn[i].innerText != "View Less"){
+//       readMoreBtn[i].innerText = "View Less";
+//     }else{
+//       readMoreBtn[i].innerText = "View Again";
+//     }
+// 
+//   });
+// 
+// 
+// }
 
 
 //readMoreBtn.addEventListener("click",(e)=>{
